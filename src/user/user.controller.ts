f095390b626ed05bd0849b6a90a29c7731b04e9c
eyesoreee <salihjohaire@gmail.com>
2026-05-18 10:45:29 +0800

    implemented create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,19 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller()
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @MessagePattern({ cmd: 'user.ping' })
   ping(): string {
     return '[User] I am alive.';
+  }
+
+  @MessagePattern({ cmd: 'user.create' })
+  create(@Payload() dto: CreateUserDto) {
+    return this.userService.create(dto);
   }
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,40 @@
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserStatus } from '../enums/user-status.enum';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(dto: CreateUserDto) {
+    const existingUser = await this.userRepository.findOneBy({
+      username: dto.username,
+    });
+
+    if (existingUser) throw new ConflictException('Username already exists.');
+
+    try {
+      const newUser = this.userRepository.create({
+        ...dto,
+        status: dto.status ?? UserStatus.ACTIVE,
+      });
+
+      const saved = await this.userRepository.save(newUser);
+
+      const { password, ...result } = saved;
+      return result;
+    } catch {
+      throw new InternalServerErrorException('Failed to create user');
+    }
+  }
+}
